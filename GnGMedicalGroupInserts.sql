8dcdaa25e1e72aee9b8d3f6f43f56dd5d052aeb9
sgsub165 <37195180+sgsub165@users.noreply.github.com>
2018-04-24 21:32:58 -0500

    tables are good and populated

---
+++ GnGMedicalGroupInserts.sql
@@ -0,0 +1,32 @@
+--insert doctors
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Robert", "Bishop");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Glenda", "Williams");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Brett", "Malcolm");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Winston", "Salem");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Dianne", "Shelton");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Ashley", "Le");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Toi", "Durant");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Sarai", "Louis");
+INSERT INTO Doctor (doctor_id, fname, lname) VALUES (NULL, "Chanda", "Mena");
+
+--insert nurses
+INSERT INTO Nurse (nurse_id, fname, lname) VALUES (NULL, "Kayle", "Grier");
+INSERT INTO Nurse (nurse_id, fname, lname) VALUES (NULL, "Bryon", "Grier");
+INSERT INTO Nurse (nurse_id, fname, lname) VALUES (NULL, "Frida", "Prentice");
+INSERT INTO Nurse (nurse_id, fname, lname) VALUES (NULL, "Yasmin", "Putman");
+
+--insert patients
+INSERT INTO Patient (patient_id, fname, lname) VALUES (NULL, "David", "Flores");
+INSERT INTO Patient (patient_id, fname, lname) VALUES (NULL, "Ruby", "Wilson");
+
+--insert medical procedures
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "General Examination", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Vaccination", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Abrasions/Contusions", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Orthopedic", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "ENT", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Cardiac", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Blood Work", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Infectious Disease", NULL);
+INSERT INTO MedicalProcedure (procedure_id, procedure_name, other_description) VALUES (NULL, "Other", NULL);
+
